6818406edd863409a3567ab5aea546ba3f276824
Le Bao Trung <trunglbth1809003@fpt.edu.vn>
2018-10-10 23:51:06 +0700

    hoan thien

--- main.c
+++ main.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int canh1;
-int canh2;
-int canh3;
+int canh1,canh2,canh3;
 
-int chuvitamgiac(canh1, canh2, canh3) {
-    return canh1 + canh2 + canh3;
-}
 
 int nuachuvitamgiac(chuvitamgiac) {
-    return chuvitamgiac / 2;
+    return (float)(canh1+canh2+canh3)/2;
 }
 
 int main() {
@@ -20,16 +15,14 @@ int main() {
     scanf("%d", &canh2);
     printf("moi nhap chieu dai canh thu ba: \n");
     scanf("%d", &canh3);
-    if (canh1 <= 0) {
-        printf("do dai 1 canh tam giac khong the nho hon hoac bang 0\n");;
-    } else if (canh2 <= 0) {
+
+    if ((canh1 <= 0)|(canh3 <= 0)|(canh3 <= 0)) {
         printf("do dai 1 canh tam giac khong the nho hon hoac bang 0\n");
-    } else if (canh3 <= 0) {
-        printf("do dai 1 canh tam giac khong the nho hon hoac bang 0\n");
+
     } else if (canh1 + canh2 == canh3) {
         printf("tong hai canh cua tam giac luon lon hon canh con lai(theo bat dang thuc tam giac)");
         exit(0);
     }
-    printf(" nua chu vi cua tam giac la: %d\n", nuachuvitamgiac((canh1 + canh2 + canh3) / 2));
+    printf(" nua chu vi cua tam giac la: %d\n", nuachuvitamgiac(canh1,canh2,canh3));
     return 0;
 }
